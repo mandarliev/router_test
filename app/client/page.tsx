0e42927e37d81c563f40ccfa998f82b28e9bdd73
Dimitar <dimitar.mandarliev@gmail.com>
2023-06-22 11:45:59 +0300

    Initial commit

---
+++ app/client/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Counter from "./counter";
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="text-2xl font-semibold">My Page</h1>
+      <Counter />
+    </section>
+  );
+}
---
+++ app/static/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <section>
+      <h1 className="text-2xl font-semibold">My Page</h1>
+      <p className="text-gray-700 mt-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas explicabo
+        dignissimos obcaecati voluptatum, ab illo qui at tempora nobis
+        recusandae optio. Repellendus exercitationem quos sint, possimus animi
+        omnis unde tenetur.
+      </p>
+    </section>
+  );
+}
